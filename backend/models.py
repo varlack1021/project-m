a317cca33a9dae869ab84acbeae1711449d8d62b
Pharez Varlack <varlack17@hotmail.com>
2020-08-07 12:22:54 -0400

    Added dependencies

--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-
+from django.conf import settings
 #We use the makemigrate command to update and or create our database
 #Migrate keeps track of any changes and then applies those changes
 # Create your models here.
 class Video(models.Model):
 	video_link = models.CharField(max_length=200)
+	user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
